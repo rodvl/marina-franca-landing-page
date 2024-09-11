72dd6ca8c195a1cd79d4c61518b1a9ea23c49556
Rodrigo Lapertosa <rodlapertosa@gmail.com>
2024-09-10 22:52:44 -0300

    feat: add layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,11 @@
 import Head from "next/head";
-import localFont from "next/font/local";
+import { Inter } from 'next/font/google'
 import styles from "@/styles/Home.module.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export default function Home() {
   return (
@@ -22,15 +16,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div
-        className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
+        className={`${styles.page} ${inter.className}`}
       >
         <main className={styles.main}>
-          <h1>oie marinex espero que funcione</h1>
-
-          <a href={"https://api.whatsapp.com/send/?phone=5531996927404&text&type=phone_number&app_absent=0"} target="_blank" download>Whatsapp</a>
-          <a href={"https://drive.google.com/file/d/1jlyzD_Ymll2qLJ1SKLY7Oo8iqthTVPaz/view?usp=drivesdk"} target="_blank">Carta de serviços</a>
+          <h1 className={`${styles.title}`}>MARINA FRANÇA BEAUTY</h1>
+          <div className={`${styles.linkContainer}`}>
+            <a className={`${styles.link}`} href={"https://api.whatsapp.com/send/?phone=5531996927404&text&type=phone_number&app_absent=0"} target="_blank" download><img className={`${styles.whatsapp}`} src="/whatsapp.svg" alt="whatsapp-logo"/> Entre em contato</a>
+            <a className={`${styles.link}`} href={"https://drive.google.com/file/d/1jlyzD_Ymll2qLJ1SKLY7Oo8iqthTVPaz/view?usp=drivesdk"} target="_blank">Carta de serviços</a>
+          </div>
 
         </main>
+        <img className={`${styles.image}`} src="/beauty.svg" alt="beauty text"/>
       </div>
     </>
   );
